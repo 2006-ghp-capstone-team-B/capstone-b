fe1f9e429692b4592071f98e7f7c09c9c2f6fbd4
ceciliachung <ceciliayu.chung@gmail.com>
2020-08-20 12:17:09 -0700

    database: build models, associations

---
+++ db/models/storePreference.js
@@ -0,0 +1,21 @@
+const Sequelize = require('sequelize')
+const db = require('../db')
+
+const StorePreference = db.define("storePreference", {
+    userId: {
+        type: Sequelize.INTEGER,
+        allowNull: false,
+        validate: {
+            notEmpty: true
+        }
+    },
+    storeId: {
+        type: Sequelize.INTEGER,
+        allowNull: false,
+        validate: {
+            notEmpty: true
+        }
+    },
+});
+
+module.exports = StorePreference;
