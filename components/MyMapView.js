9e9c6fd76b16983f876a7fa94bdba0d4e5ea5837
Ariel Wainer <arielwainer4@gmail.com>
2020-08-21 15:47:04 -0400

    Feat: Functional map and search bar

--- components/MyMapView.js
+++ components/MyMapView.js
@@ -1,21 +1,16 @@
 import React from "react";
-// import { MapView } from "expo";
-import MapView from "react-native-maps";
-// import MapView, { Marker } from "expo";
-import { View } from "react-native";
+import MapView, {Marker} from "react-native-maps";
 const MyMapView = (props) => {
-  console.log(props, "my map view props");
+  console.log( "~~~~~~~my map view props~~~~~~~", props);
   return (
-    <View>
       <MapView
         style={{ flex: 1 }}
         region={props.region}
         showsUserLocation={true}
         onRegionChange={(reg) => props.onRegionChange(reg)}
       >
-        {/* <Marker coordinate={props.region} /> */}
+        <Marker coordinate={props.region} />
       </MapView>
-    </View>
   );
 };
 export default MyMapView;
