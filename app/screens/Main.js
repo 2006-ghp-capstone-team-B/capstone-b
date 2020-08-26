d70850f9b52e61c315c03ae5598692a5865089d5
ceciliachung <ceciliayu.chung@gmail.com>
2020-08-26 12:27:19 -0700

    temp: added Households button

--- app/screens/Main.js
+++ app/screens/Main.js
@@ -2,15 +2,15 @@ import React from "react";
 import { createBottomTabNavigator } from '@react-navigation/bottom-tabs';
 import { NavigationContainer } from "@react-navigation/native";
 import { MaterialCommunityIcons, AntDesign, FontAwesome, Fontisto } from 'react-native-vector-icons';
-import {Dashboard, UserProfile, Notifications,HouseholdProfile } from "../components"
+import { Dashboard, UserProfile, Notifications, HouseholdProfile } from "../components"
 import MapContainer from "./MapContainer";
 
 const BottomTab = createBottomTabNavigator();
 
 export default function Main() {
   return (
-      <NavigationContainer>
-        <BottomTab.Navigator initialRouteName="Dashboard" tabBarOptions={{ activeTintColor: '#e91e63', }}>
+    <NavigationContainer>
+      <BottomTab.Navigator initialRouteName="Dashboard" tabBarOptions={{ activeTintColor: '#e91e63', }}>
         <BottomTab.Screen name="Dashboard" component={Dashboard} options={{
           tabBarIcon: ({ color, size }) => (
             <MaterialCommunityIcons name="home" color={color} size={size} />
@@ -24,6 +24,11 @@ export default function Main() {
         <BottomTab.Screen name="My Stores" component={MapContainer} options={{
           tabBarIcon: ({ color, size }) => (
             <Fontisto name="shopping-store" color={color} size={size} />
+          ),
+        }} />
+        <BottomTab.Screen name="My Household" component={HouseholdProfile} options={{
+          tabBarIcon: ({ color, size }) => (
+            <FontAwesome name="users" color={color} size={size} />
           ),
         }} />
         <BottomTab.Screen name="Me" component={UserProfile} options={{
