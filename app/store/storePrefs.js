f681d8fd4a2219230abe0219091f3b4bb027a300
Taihua Rubin <TaihuaRubin@gmail.com>
2020-08-27 17:13:49 -0400

    FEAT: added delete routes in api/stores and also storePrefs

--- app/store/storePrefs.js
+++ app/store/storePrefs.js
@@ -2,11 +2,17 @@ import axios from "axios";
 import { MY_IP } from "../../secret";
 
 const GET_STORE_PREFS = "GET_STORE_PREFS";
+const DELETE_STORE_FROM_PREFS = "DELETE_STORE_FROM_PREFS";
 
 const initialState = [];
 
 const getStorePrefs = (storePrefs) => ({
   type: GET_STORE_PREFS,
+  storePrefs,
+});
+
+const deleteStoreFromPrefs = (storePrefs) => ({
+  type: DELETE_STORE_FROM_PREFS,
   storePrefs,
 });
 
@@ -20,9 +26,20 @@ export const fetchStorePrefs = (userId) => async (dispatch) => {
   }
 };
 
+export const deleteStoreThunk = (userId, storeId) => async (dispatch) => {
+  try {
+    const res = await axios.delete(`http://${MY_IP}:19006/api/stores/${userId}`, storeId);
+    dispatch(DELETE_STORE_FROM_PREFS(res.data));
+  } catch (e) {
+    console.log(e);
+  }
+};
+
 export default function (state = initialState, action) {
   switch (action.type) {
     case GET_STORE_PREFS:
+      return action.storePrefs;
+    case DELETE_STORE_FROM_PREFS:
       return action.storePrefs;
     default:
       return state;
--- server/api/stores.js
+++ server/api/stores.js
@@ -1,7 +1,6 @@
 const router = require("express").Router();
 const { User, ListAccess, List, ItemUserList, Item, StorePreference, Store } = require("../db/models");
 
-
 router.get("/", async (req, res, next) => {
   try {
     const allStores = await StorePreference.findAll();
@@ -26,19 +25,37 @@ router.get("/:userId", async (req, res, next) => {
 router.post("/:userId", async (req, res, next) => {
   try {
     const [store, createdStore] = await Store.findOrCreate({
-      where:{
+      where: {
         storeName: req.body.storeName,
         latitude: req.body.latitude,
         longitude: req.body.longitude,
-        category: req.body.category
-      }})
+        category: req.body.category,
+      },
+    });
     const [storePref, createdStorePref] = await StorePreference.findOrCreate({
       where: {
         storeId: store.id,
-        userId: req.params.userId
-      }
-    })
+        userId: req.params.userId,
+      },
+    });
     res.status(201).json(storePref);
+  } catch (error) {
+    next(error);
+  }
+});
+
+// delete the store from StorePreference
+// but have it still be in Store
+router.delete("/:userId", async (req, res, next) => {
+  try {
+    await StorePreference.destroy({ where: { storeId: req.body.storeId } });
+
+    // get updated StorePref list
+    const updatedStorePrefs = await StorePreference.findAll({
+      where: { userId: req.params.userId },
+      include: { model: Store },
+    });
+    res.json(updatedStorePrefs);
   } catch (error) {
     next(error);
   }
